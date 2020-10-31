e93e9b7cdc874eb112b860c6eb28a91a419a8ddc
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-10-31 18:10:17 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "Nikita@123", "vinterns")
+    $con = mysqli_connect("localhost", "root", "", "vinterns")
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();
