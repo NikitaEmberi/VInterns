dc62f083d5c0820c13d5789a95e8f231107b8d0d
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-11-01 11:15:09 +0530

    Update student-data.php

--- dist/student-data.php
+++ dist/student-data.php
@@ -13,31 +13,26 @@
     <title>VIntern</title>
 </head>
 <body class = "bg-gray-900">
-  <header class=" text-gray-500 bg-gray-800 body-font">
+    <header class=" text-gray-500 bg-gray-800 body-font">
     <div class="container mx-auto flex flex-wrap px-5 py-6 flex-col md:flex-row items-center">
-    <a href="index.php" class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+    <a href="index.php" class="flex title-font font-medium items-center text-white mb-4 mr-auto md:mb-0">
               <img class="logo-img" src="../images/logoresize.png">
                 <span class="ml-3 text-xl">vIntern</span>
               </a>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <nav class="md:ml-auto mr-auto flex flex-wrap items-center text-base justify-center">
             <a class="mr-5 hover:text-white">ADMIN LOGIN</a>
             </nav>
-            <a href="sort-students-form.php?branch=<?php echo $_GET['branch']?>"><button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">SORT
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M1 12h14M13 5l7 7-7 7"></path>
-            </svg>
-          </button></a>
+          <div class="md:ml-auto md:mr-2">
+              <a href="sort-students-form.php?branch=<?php echo $_GET['branch'] ?>"><button class='flex mx-auto  text-white bg-teal-500 border-0   px-2  py-2 ocus:outline-none hover:bg-teal-600 rounded text-lg'>
+              <i class="fa fa-filter"> Filter </i></button></a>
+          </div>
+          <a href="branch-select.php"><button class='flex mx-auto  text-white bg-teal-500 border-0   px-2  py-2 ocus:outline-none hover:bg-teal-600 rounded text-lg'>
+              <i class="fa fa-sign-out"> BACK</i></button>
+          </a>
     </div>
   </header>
 
-    <!-- <section class='text-gray-500 bg-gray-900 body-font overflow-hidden'>
-        <div class='container px-20 py-24 mx-auto'>
-            <div class='flex flex-col'>
-                <h1 class='text-center w-full sm:text-3xl text-2xl font-medium title-font pt-6 text-white'>Students of branch <?php echo $_GET['branch']?></h1>
-            </div>
-        </div>
-    </section> -->
-
+   
   <section class="text-gray-500 bg-gray-900 body-font justify-center" style="margin-left:auto">
         <div class='container px-20 py-2 mx-auto'>
             <div class='flex flex-col text-center w-full mb-5'>
